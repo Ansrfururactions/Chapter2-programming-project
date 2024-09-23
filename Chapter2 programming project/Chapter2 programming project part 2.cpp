0aeb70f20474358606e61f3ea01115fbff904b68
Ansrfururactions <careybarberjr@gmail.com>
2024-09-23 18:47:07 -0400

    Update Chapter2 programming project part 2.cpp

--- Chapter2 programming project/Chapter2 programming project part 2.cpp
+++ Chapter2 programming project/Chapter2 programming project part 2.cpp
@@ -10,4 +10,5 @@ int main()
     double gallon = 20, towndist = gallon * 23.5, highwaydist = gallon * 28.9;
 
     std::cout << "the car can travel a distance of " << towndist << " miles in a town, and " << highwaydist << " miles on the highway." << endl;
+    return 0;
 }
