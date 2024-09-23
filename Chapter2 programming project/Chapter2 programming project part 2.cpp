0c0ccfe8cf9187dc27e3528534361554dd18f689
Ansrfururactions <careybarberjr@gmail.com>
2024-09-23 18:40:38 -0400

    Create Chapter2 programming project part 2.cpp

---
+++ Chapter2 programming project/Chapter2 programming project part 2.cpp
@@ -0,0 +1,13 @@
+// Carey Barber
+// 9/23/2024
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double gallon = 20, towndist = gallon * 23.5, highwaydist = gallon * 28.9;
+
+    std::cout << "the car can travel a distance of " << towndist << " miles in a town, and " << highwaydist << " miles on the highway." << endl;
+}
