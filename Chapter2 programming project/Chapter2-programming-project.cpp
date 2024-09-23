a4eab3c7f7859820d8a32329f2f69eb764711173
Ansrfururactions <careybarberjr@gmail.com>
2024-09-23 12:48:51 -0400

    Create Chapter2-programming-project.cpp

---
+++ Chapter2 programming project/Chapter2-programming-project.cpp
@@ -0,0 +1,14 @@
+// Carey Barber
+// 9/23/2024
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double fiveyroceanlvls=7.5, svnyroceanlvls=10.5, tenyroceanlvls=15;
+
+    std::cout << "The ocean level will be " << fiveyroceanlvls << " millimeters higher in five years, " << svnyroceanlvls << " millimeters higher in seven years, and " << tenyroceanlvls<< " millimeters higher in ten years" << endl;
+    return 0;
+}
